3b5a1ee0d14c1ba2fb237e932d869e87e13c27cb
이홍준 <zunza96@naver.com>
2022-04-28 23:57:49 +0900

    chore : 3번쨰 배너

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import styled from "@emotion/styled";
+import styled from "@emotion/styled/macro";
 import { Link } from "react-router-dom";
 
 function HomePage() {
@@ -12,11 +12,11 @@ function HomePage() {
               <br />
               당근마켓
             </h1>
-            <p className="text-m">
+            <MainText>
               중고 거래부터 동네 정보까지, 이웃과 함께해요.
               <br />
               가깝고 따뜻한 당신의 근처를 만들어요.
-            </p>
+            </MainText>
           </div>
           <HomeMainImageTop>
             <img
@@ -43,19 +43,55 @@ function HomePage() {
               <br />
               중고 직거래 마켓
             </h1>
-            <p className="text-m">
+            <MainText>
               동네 주민들과 가깝고 따뜻한 거래를 지금 경험해보세요.
-            </p>
+            </MainText>
             <div className="home-buttons">
-              <Link to ="/" className="home-button text-l text-bold">인기매물 보기</Link>
-              <Link to ="/" className="home-button text-l text-bold ml-3">믿을 수 있는 중고거래</Link>
+              <Link to="/" className="home-button text-l text-bold">
+                인기매물 보기
+              </Link>
+              <Link to="/" className="home-button text-l text-bold ml-3">
+                믿을 수 있는 중고거래
+              </Link>
             </div>
           </div>
         </HomeMainContent>
       </HomeMainSection>
+      <HomeMainGreenSection>
+        <HomeMainReverseContent>
+          <HomeMainImage02>
+            <img
+              srcSet="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/image-2-91a2286453bdf82dea16a7f0ee4ceb9dd325eae0e5a2a9967ba72c344bf8f2fc.webp "
+              loading="lazy"
+              alt="이웃과 함께하는 동네생활"
+              src="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/image-2-f286322ab98acedf914a05bf77e84c67dcb897c8ccb543e66f6afea9d366d06d.png"
+            />
+          </HomeMainImage02>
+          <div>
+            <h1 className="home-main-title">
+              이웃과 함께 하는
+              <br />
+              동네생활
+            </h1>
+            <MainText>
+              우리 동네의 다양한 이야기를 이웃과 함께 나누어요.
+            </MainText>
+            {/* <ul className="home-story-list">
+              <li className="home-story-list-item"></li>
+              <li className="home-story-list-item"></li>
+              <li className="home-story-list-item"></li>
+            </ul> */}
+          </div>
+        </HomeMainReverseContent>
+      </HomeMainGreenSection>
     </>
   );
 }
+const MainText = styled.p`
+  font-size: 1.6rem !important;
+  line-height: 1.5;
+  letter-spacing: -0.3px;
+`
 const HomeMainSectionTop = styled.section`
   padding-top: 24px;
   background-color: #fbf7f2;
@@ -66,7 +102,7 @@ const HomeMainSectionTop = styled.section`
     margin-bottom: 3.2rem;
     letter-spacing: -0.4px;
   }
-  .text-m {
+  ${MainText} {
     font-size: 1.6rem !important;
     line-height: 1.5;
     letter-spacing: -0.3px;
@@ -103,20 +139,20 @@ const HomeMainImageTop = styled.div`
 
 const HomeMainSection = styled.section`
   padding: 6rem 0;
-  .home-title{
+  .home-title {
     font-size: 4rem;
     line-height: 1.35;
     margin-bottom: 2.4rem;
   }
-  .text-m{
+  .text-m {
     font-size: 1.6rem !important;
     line-height: 1.5;
     letter-spacing: -0.3px;
   }
-  .home-buttons{
+  .home-buttons {
     margin-top: 3.2rem;
-    .home-button{
-      background-color: #F1F3F5;
+    .home-button {
+      background-color: #f1f3f5;
       color: #212529;
       border: none;
       border-radius: 6px;
@@ -124,22 +160,22 @@ const HomeMainSection = styled.section`
       padding: 1.45rem 2.6rem;
       display: inline-block;
     }
-    .home-button:hover{
-      background-color: #E9ECEF;
+    .home-button:hover {
+      background-color: #e9ecef;
     }
-    a ~ a{
+    a ~ a {
       margin-left: 1.6rem;
     }
   }
-  .text-l{
+  .text-l {
     font-size: 1.8rem !important;
     line-height: 1.47;
     letter-spacing: -0.3px;
   }
-  .text-bold{
+  .text-bold {
     font-weight: bold;
   }
-  .ml-3{
+  .ml-3 {
     margin-left: 1.6rem;
   }
 `;
@@ -163,5 +199,42 @@ const HomeMainImage = styled.div`
   }
 `;
 
+const HomeMainGreenSection = styled(HomeMainSection)`
+  background-color: rgb(230, 243, 230);
+  ul li{
+    margin: 1.6rem;
+  }
+  .home-main-title {
+    font-size: 4rem;
+    line-height: 1.35;
+    margin-bottom: 2.4rem;
+  }
+  .home-story-list{
+    margin-top: 4rem;
+    display: flex;
+    justify-content: space-between;
+    list-style-type: none;
+    .home-story-list-item{
+      margin-right: 4rem;
+      margin-left: 0;
+      max-width: 14rem;
+    }
+  }
+`;
+const HomeMainReverseContent = styled(HomeMainContent)`
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+`;
+const HomeMainImage02 = styled(HomeMainImage)`
+  background-size: 546px 740px;
+  width: 546px;
+  height: 740px;
+  img {
+    background-size: 546px 740px;
+    width: 546px;
+    height: 740px;
+  }
+`;
 
 export default HomePage;
